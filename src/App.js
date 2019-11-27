4a8af0be63ad4e72acd2bb611cf57f373d66d187
rashika.singhh <rashika.singhh@hotmail.com>
2019-11-27 20:02:58 +0530

    url changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShareButton from 'react-web-share-button';
 function App () {
     return(
       <div style={{padding:150}}>
-        <ShareButton title="My Great Page" text="A really great page" url="http://www.greatpage.com" />
+        <ShareButton title="Testing Sharing" text="Heroku deployed" url="https://webshare-api.herokuapp.com/"/>
       <FacebookShareButton url={'https://wwww.facebook.com'}>
         <FacebookIcon/>
         </FacebookShareButton>
